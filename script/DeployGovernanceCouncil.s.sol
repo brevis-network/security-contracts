e1f8e1b563421332910706a4d91136c19e81b370
Xiaozhou Li <xl@cs.princeton.edu>
2025-10-25 00:30:19 -0700

    update deployment scripts

--- script/DeployGovernanceCouncil.s.sol
+++ script/DeployGovernanceCouncil.s.sol
@@ -24,8 +24,7 @@ import {GovernanceCouncil} from "src/governance/GovernanceCouncil.sol";
  *   "powers": [1, 1],
  *   "forwarders": ["0x..."],
  *   "activePeriod": 86400,
- *   "quorumThreshold": 60,
- *   "fastPassThreshold": 50
+ *   "quorumThreshold": 60
  * }
  */
 contract DeployGovernanceCouncil is Script {
@@ -44,7 +43,6 @@ contract DeployGovernanceCouncil is Script {
         address[] memory forwarders = json.readAddressArray(".forwarders");
         uint256 activePeriod = json.readUint(".activePeriod");
         uint256 quorumThreshold = json.readUint(".quorumThreshold");
-        uint256 fastPassThreshold = json.readUint(".fastPassThreshold");
 
         require(voters.length > 0, "Deploy: no voters");
         require(voters.length == powers.length, "Deploy: voters/powers length mismatch");
